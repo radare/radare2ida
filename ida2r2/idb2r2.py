08eee1379dd076ce2879cb403dafb3ab24aafde2
Itay Cohen <itaycohen23@gmail.com>
2018-04-25 20:13:21 +0300

    Minor improvements and bug fixes
    [*] Fixed issue with functions that have "@" in their name
    [*] Fixed extra blank output of empty flags
    [*] Added detection of the base address
    [*] Print instruction of how to execute r2 with the new script and the base address

--- ida2r2/idb2r2.py
+++ ida2r2/idb2r2.py
@@ -8,7 +8,8 @@ def idb2r2_comments(api, textseg):
     for ea in range(textseg, api.idc.SegEnd(textseg)):
         try:
             flags = api.ida_bytes.get_cmt(ea, True)
-            print("CCu base64:" + base64.b64encode(flags.encode(encoding='UTF-8')).decode("utf-8") + " @ " + str(ea))
+            if flags != "":
+                print("CCu base64:" + base64.b64encode(flags.encode(encoding='UTF-8')).decode("utf-8") + " @ " + str(ea))
         except Exception as e:
             try:
                 flags = api.ida_bytes.get_cmt(ea, False)
@@ -18,11 +19,13 @@ def idb2r2_comments(api, textseg):
 
 def idb2r2_functions(api):
     for ea in api.idautils.Functions():
-        print("af " + api.idc.GetFunctionName(ea) + " @ " + str(ea))
+        print("af " + api.idc.GetFunctionName(ea).replace("@","_") + " @ " + str(ea))
 
 with idb.from_file(sys.argv[1]) as db:
     api = idb.IDAPython(db)
+    baddr = api.ida_nalt.get_imagebase()
     idb2r2_functions(api)
     segs = idb.analysis.Segments(db).segments
     for segment in segs.values():
         idb2r2_comments(api,segment.startEA)
+    sys.stderr.write("Convertion done.\nExecute: r2 -i [output.r2] -B %s [filename]\n" % (hex(baddr)))
